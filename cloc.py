a7e6bb57fc8e043fcf8d00873dabc3bf890ea922
brian <brianbarbieri@hotmail.com>
2024-11-24 13:04:43 +0100

    Added table to readme

--- cloc.py
+++ cloc.py
@@ -79,12 +79,12 @@ def visualize_info(info_dict):
     total_files: int = sum(info["# of files"] for info in info_dict.values())
 
     print("-------------------------------------------------------------------------------")
-    print(f"{'Extension':20} {'# of files':20} {'# of empty lines':20} {'line count':20}")
+    print(f"{'Extension':20} {'# of files':20} {'# of lines':20} {'# of empty lines':20}")
     print("-------------------------------------------------------------------------------")
     for ext, data in sorted_info:
-        print(f"{ext:20} {data['# of files']:<20} {data['empty_lines']:<20} {data['line_count']:<20}")
+        print(f"{ext:20} {data['# of files']:<20} {data['line_count']:<20} {data['empty_lines']:<20}")
     print("-------------------------------------------------------------------------------")
-    print(f"{'Sum:':20} {total_files:<20} {total_empty_lines:<20} {total_line_count:<20}")
+    print(f"{'Sum:':20} {total_files:<20} {total_line_count:<20} {total_empty_lines:<20}")
     print("-------------------------------------------------------------------------------")
     
 def show_empty_lines(directory, git_ignore_path):    
